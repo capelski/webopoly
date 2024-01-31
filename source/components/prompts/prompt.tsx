38f90ba15a7354443f2a4fde6ce70c008d2ccf1b
Carles Capellas <capellas.carles@gmail.com>
2024-01-31 11:31:56 +0100

    Remove no longer necessary prompt map separate file

--- source/components/prompts/prompt.tsx
+++ source/components/prompts/prompt.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { GamePhase } from '../../enums';
+import { GamePhase, PromptType } from '../../enums';
 import { Game, Prompt } from '../../types';
 import { Modal } from '../common/modal';
+import { AnswerOfferPrompt } from './answer-offer-prompt';
+import { CannotPayPrompt } from './cannot-pay-prompt';
+import { CardPrompt } from './card-prompt';
+import { GoToJailPrompt } from './go-to-jail-prompt';
+import { JailOptionsPrompt } from './jail-options-prompt';
+import { PlayerWinsPrompt } from './player-wins-prompt';
 import { PromptInterface } from './prompt-interface';
-import { promptsMap } from './prompts-map';
+
+const promptsMap: {
+  [TKey in PromptType]: PromptInterface<TKey>;
+} = {
+  [PromptType.answerOffer]: AnswerOfferPrompt,
+  [PromptType.cannotPay]: CannotPayPrompt,
+  [PromptType.card]: CardPrompt,
+  [PromptType.goToJail]: GoToJailPrompt,
+  [PromptType.jailOptions]: JailOptionsPrompt,
+  [PromptType.playerWins]: PlayerWinsPrompt,
+};
 
 interface PromptComponentProps {
   game: Game;
